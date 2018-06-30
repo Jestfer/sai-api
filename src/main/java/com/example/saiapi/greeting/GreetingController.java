c0218f7a959d538d677055ed099892d78ffc4c59
Josué Estévez Fernández <jestfer6@gmail.com>
2018-06-30 23:48:31 +0100

    refactored new controller class files into new individual packages, TopicController to return JSON object

--- src/main/java/com/example/saiapi/greeting/GreetingController.java
+++ src/main/java/com/example/saiapi/greeting/GreetingController.java
@@ -1,4 +1,4 @@
-package com.example.saiapi;
+package com.example.saiapi.greeting;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
---
+++ src/main/java/com/example/saiapi/topic/TopicController.java
@@ -0,0 +1,4 @@
+package com.example.saiapi.topic;
+
+public class TopicController {
+}
