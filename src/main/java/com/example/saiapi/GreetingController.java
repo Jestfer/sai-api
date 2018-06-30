37bdcc96546863de513657a9f646db0938c21f59
Josué Estévez Fernández <jestfer6@gmail.com>
2018-06-30 23:40:51 +0100

    rendering string on GreetingController for now, next: return objects from controller

--- src/main/java/com/example/saiapi/GreetingController.java
+++ src/main/java/com/example/saiapi/GreetingController.java
@@ -1,8 +1,13 @@
 package com.example.saiapi;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
 
+   @RequestMapping("/hello")
+   public String sayHi() {
+      return "Hi!";
+   }
 }
