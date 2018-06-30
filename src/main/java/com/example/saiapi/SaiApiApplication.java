58800668a56542f2522246af14b8f8c44f23034b
Josué Estévez Fernández <jestfer6@gmail.com>
2018-06-30 20:35:49 +0100

    bootstrapped project using Spring Boot Initializr

---
+++ src/main/java/com/example/saiapi/SaiApiApplication.java
@@ -0,0 +1,12 @@
+package com.example.saiapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaiApiApplication.class, args);
+	}
+}
