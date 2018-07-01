3b52b93e0a2aaca5b1e6a149a7a6d7eda933fc99
Josué Estévez Fernández <jestfer6@gmail.com>
2018-07-01 09:21:15 +0100

    created a list of topics in TopicController as an Array list, object created using the Topic model

--- src/main/java/com/example/saiapi/topic/TopicController.java
+++ src/main/java/com/example/saiapi/topic/TopicController.java
@@ -3,11 +3,18 @@ package com.example.saiapi.topic;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 public class TopicController {
 
    @RequestMapping("/topics")
-   public String getAllTopics() {
-      return "All Topics";
+   public List<Topic> getAllTopics() {
+      return Arrays.asList(
+              new Topic("spring", "Spring Framework", "Spring Framework Desc"),
+              new Topic("java", "Core Java", "Core Java Desc"),
+              new Topic("javascript", "JavaScript", "JavaScript Desc")
+      );
    }
 }
