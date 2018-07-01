94e044e129b6ece46efff30b306d69be27fddf60
Josué Estévez Fernández <jestfer6@gmail.com>
2018-07-01 09:06:06 +0100

    generated Topic class with id, name and description properties for topics. Also generated getters and setters

--- src/main/java/com/example/saiapi/topic/TopicController.java
+++ src/main/java/com/example/saiapi/topic/TopicController.java
@@ -1,4 +1,13 @@
 package com.example.saiapi.topic;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TopicController {
+
+   @RequestMapping("/topics")
+   public String getAllTopics() {
+      return "All Topics";
+   }
 }
