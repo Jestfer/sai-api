344f6fca958271d7d2fa0e6c86c37e7502e931d5
Josué Estévez Fernández <jestfer6@gmail.com>
2018-07-01 09:11:08 +0100

    generated constructor with different properties as args, also no-args constructor

--- src/main/java/com/example/saiapi/topic/Topic.java
+++ src/main/java/com/example/saiapi/topic/Topic.java
@@ -6,6 +6,17 @@ public class Topic {
    private String name;
    private String description;
 
+   // The object is easier to initialize with a no-args constructor
+   public Topic() {
+
+   }
+
+   public Topic(String id, String name, String description) {
+      this.id = id;
+      this.name = name;
+      this.description = description;
+   }
+
    public String getId() {
       return id;
    }
