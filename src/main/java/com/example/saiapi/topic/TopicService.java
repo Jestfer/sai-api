b0053eb1475b7a7aaf94ea975e236f954bd9ec6d
Josué Estévez Fernández <jestfer6@gmail.com>
2018-07-02 21:56:48 +0100

    Added method to allow POST to mutable instance of Arrays.asList, tested with POSTMAN

--- src/main/java/com/example/saiapi/topic/TopicService.java
+++ src/main/java/com/example/saiapi/topic/TopicService.java
@@ -2,17 +2,18 @@ package com.example.saiapi.topic;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class TopicService {
 
-   private List<Topic> topics = Arrays.asList(
+   private List<Topic> topics = new ArrayList<>(Arrays.asList(
       new Topic("spring", "Spring Framework", "Spring Framework Desc"),
       new Topic("java", "Core Java", "Core Java Desc"),
       new Topic("javascript", "JavaScript", "JavaScript Desc")
-   );
+   ));
 
    public List<Topic> getAllTopics() {
       return topics;
@@ -20,5 +21,9 @@ public class TopicService {
 
    public Topic getTopic(String id) {
       return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get(); // Lambda...
+   }
+
+   public void addTopic(Topic topic) {
+      topics.add(topic);
    }
 }
